3e96d738307ff69411fd09ab6207756413e9e122
shimekeleme <shimekeleme@gmail.com>
2024-04-25 09:14:57 +0000

    fire

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,12 +1,27 @@
 #!/usr/bin/python3
+""" Script that computes a minimum operations
+    needed in a CopyAll - Paste task
 """
-Main file for testing
-"""
 
-minOperations = __import__('0-minoperations').minOperations
 
-n = 4
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+def minOperations(n):
+    """
+    Method for compute the minimum number
+    of operations for task Copy All and Paste
 
-n = 12
-print("Min # of operations to reach {} char: {}".format(n, minOperations(n)))
+    Args:
+        n: input value
+        factor_list: List to save the operations
+    Return: the sum of the operations
+    """
+    if n < 2:
+        return 0
+    factor_list = []
+    i = 1
+    while n != 1:
+        i += 1
+        if n % i == 0:
+            while n % i == 0:
+                n /= i
+                factor_list.append(i)
+    return sum(facto
